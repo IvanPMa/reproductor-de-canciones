63051a6902abc71ad9fd5bff8f292e5bb07b38ec
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-06-17 14:14:44 -0500

    reproductor

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const playSong = document.getElementsByClassName('play');
 const stopSong = document.getElementsByClassName('stop');
 const volume = document.querySelector('.volume');
+const nameSong = document.querySelector('.name');
 let audio;
 
 for(element of playSong){
@@ -8,12 +9,14 @@ for(element of playSong){
         let cancion = this.getAttribute('id');
         audio = new Audio(`./audios/${cancion}.mp3`);
         audio.play();
+        nameSong.innerHTML = `Reproduciendo ${cancion}`;
     });
 }
 
 for(element of stopSong){
     element.addEventListener('click', function(){
         audio.pause();
+        nameSong.innerHTML = `Detenido`
     });
 }
 
